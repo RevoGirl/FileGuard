503331bc0f4f3d580faa5876829fc1ce005a5d12
Samantha <dutchhockeygoalie@yahoo.com>
2012-02-17 19:46:56 +0100

    Replacing old script with a more streamlined one

--- scripts/makeFG.sh
+++ scripts/makeFG.sh
@@ -4,201 +4,202 @@
 # Script (makeFG.sh) to create the FileGuard directory structure, 
 # and to backup certain essential files required by OS X.
 #
-# Version 0.5 - Copyright (c) 2012 by RevoGirl <DutchHockeyGoalie@yahoo.com>
+# Version 0.7 - Copyright (c) 2012 by RevoGirl <DutchHockeyGoalie@yahoo.com>
 #
 
+#set -x # Used for tracing errors
+
+#============================== CONFIGURATION VAR ===============================
+
+LAYOUT=892 # We need to read this from ioreg output.
+
+#================================= GLOBAL VARS ==================================
+
+fgDaemonDir=/Extra2/FileGuard/Daemon
+
+fgDirectories=( /Extra2 FileGuard Files System Library Extensions )
+
+EXTENSIONS_DIR=/System/Library/Extensions/
+
+FILEGUARD_FILES=/Extra2/FileGuard/Files  # Do <em>not</em> add a forward slash!
+FILEGUARD_EXTENSIONS=${FILEGUARD_FILES}$EXTENSIONS_DIR
+
+#--------------------------------------------------------------------------------
 #
-# This script can be run from any place so we have to start by checking 
-# the source path. So that it will do what we expect it to do for us.
+# This is where you add your watch targets (keep an eye on the index)!
 #
+#--------------------------------------------------------------------------------
 
-# Setting the default layout number
+fgWatchTargets[0]=/boot
+fgWatchTargets[1]=/usr/standalone/i386/boot
 
-echo "\nStart\n"
+fgWatchTargets[2]=AppleHDA.kext/Contents/MacOS/AppleHDA
+fgWatchTargets[3]=AppleHDA.kext/Contents/Resources/layout${LAYOUT}.xml
+fgWatchTargets[4]=AppleHDA.kext/Contents/Resources/Platforms.xml
+fgWatchTargets[5]=AppleHDA.kext/Contents/PlugIns/AppleHDAHardwareConfigDriver.kext/Contents/Info.plist
 
-if [ $# == 1 ];
-    then
+fgWatchTargets[6]=AppleIntelCPUPowerManagement.kext/Contents/MacOS/AppleIntelCPUPowerManagement
+
+fgWatchTargets[7]=ATI6000Controller.kext/Contents/MacOS/ATI6000Controller
+
+fgWatchTargets[8]=FakeSMC.kext
+
+fgWatchTargets[9]=IONetworkingFamily.kext/Contents/PlugIns/AppleIntelE1000e.kext
+fgWatchTargets[10]=IONetworkingFamily.kext/Contents/PlugIns/AppleYukon2.kext
+
+#=============================== LOCAL FUNCTIONS ================================
+
+function _fileExists()
+{
+  if [ -e $1 ]; 
+      then
+          echo 1 # "File exists"
+      else
+          echo 0 # "File does not exist"
+  fi
+}
+
+#--------------------------------------------------------------------------------
+
+function _addFileToStorage()
+{
+  # Strip filename from path.
+  local TARGET_PATH=${2%/*}
+
+  # Check target path (directory).
+  if [ ! -d $TARGET_PATH ]; then
+      # Target path not found (add directory).
+      `/usr/bin/sudo /bin/mkdir -p $TARGET_PATH`
+  fi
+
+  if [ -s $1 ];
+      then
+          # Copy file from the OS X location to /Extra/FileGuard/Files/..
+          `/usr/bin/sudo /bin/cp -Rp $1 $2`
+
+          echo "File added to FileGuard: $2"
+      else
+          echo "NOTICE: File with zero length skipped: $1"
+  fi
+}
+
+#--------------------------------------------------------------------------------
+
+function _checkWatchTarget()
+{
+  local PATH=$1
+
+  # A path starting with a forward slash must be followed (full path given).
+  if [[ $PATH =~ ^/ ]];
+      then # Full path given (follow it).
+          local SOURCE_FILE=$PATH
+          local TARGET_FILE=${FILEGUARD_FILES}$PATH
+      else # Assume Extensions directory.
+          local SOURCE_FILE=${EXTENSIONS_DIR}$PATH
+          local TARGET_FILE=${FILEGUARD_EXTENSIONS}$PATH
+  fi
+
+  if [ $(_fileExists $TARGET_FILE) -eq 0 ];
+      then
+          if [ $(_fileExists $SOURCE_FILE) -eq 1 ];
+              then
+                  echo $(_addFileToStorage $SOURCE_FILE $TARGET_FILE)
+              else
+                  echo "ERROR: path error for: $SOURCE_FILE"
+          fi
+      else
+         echo "File checked/found: $TARGET_FILE"
+  fi
+}
+
+#--------------------------------------------------------------------------------
+#
+# Selecting layout-id for AppleHDA.
+#
+#--------------------------------------------------------------------------------
+
+function _setLayoutID()
+{
+  if [ $# == 1 ];
+      then
         LAYOUT=$1
-        echo "Using the given layout ($1) for AppleHDA."
+        echo "Using the given layout ($1) for AppleHDA.\n"
     else
         LAYOUT=892
-        echo "Using the default layout (892) for AppleHDA."
+        echo "Using the default layout (892) for AppleHDA.\n"
+  fi
+}
+
+#--------------------------------------------------------------------------------
+#
+# Check the FileGuard directory structure (adds missing directories).
+#
+#--------------------------------------------------------------------------------
+
+function _checkDirectories()
+{
+  for dir in "${fgDirectories[@]}"
+  do
+    echo "Checking directory: $dir"
+
+    # Check target directory.
+    if [ ! -d "$dir" ]; then
+      sudo mkdir $dir
+    fi
+
+    cd $dir
+  done
+
+  if [ ! -d "$fgDaemonDir" ]; then
+    sudo mkdir $fgDaemonDir
+  fi
+}
+
+#--------------------------------------------------------------------------------
+#
+# Only administrators (root) can run this script - hence the check for it here.
+#
+#--------------------------------------------------------------------------------
+
+function isRoot()
+{
+  if [ "$(id -u)" != "0" ]; then
+      echo "This script must be run as root" 1>&2
+      exit 1
+  fi
+
+  echo 1
+}
+
+#--------------------------------------------------------------------------------
+
+function main()
+{
+  _setLayoutID $1
+  _checkDirectories
+
+  echo "\nFileGuard makeFG.sh - check started on" `date "+%d-%m-%Y @ %H:%M:%S"`
+  echo "------------------------------------------------------------"
+
+  for target in "${fgWatchTargets[@]}"
+    do
+      #echo "Checking directory: $target"
+
+      _checkWatchTarget $target
+    done
+
+  echo "------------------------------------------------------------"
+  echo "Done\n"
+
+  #cd /Extra2
+}
+
+#==================================== START =====================================
+
+if [ $(isRoot) ]; then
+  main $1
 fi
 
-echo "Checking/creating directory structure..."
+#================================================================================
 
-if [ ! -d "/Extra" ]; then
-    sudo mkdir /Extra
-fi
-
-cd /Extra
-
-#
-# Here the actual work starts.
-#
-
-if [ ! -d "FileGuard" ]; then
-    sudo mkdir FileGuard
-fi
-
-cd FileGuard
-
-if [ ! -d "Deamon" ]; then
-    sudo mkdir Daemon
-fi
-
-#
-# Copy FileGuard daemon.
-#
-
-# To be added at a later date.
-
-if [ ! -d "Files" ]; then
-    sudo mkdir Files
-fi
-
-cd Files
-
-if [ ! -d "System" ]; then
-    sudo mkdir System
-fi
-
-#
-# Keeping a copy of the boot loader.
-#
-
-if [ ! -d "boot" ]; then
-    sudo cp -p /boot .
-fi
-
-#
-# Next target: Kexts.
-#
-
-cd System
-
-if [ ! -d "Library" ]; then
-    sudo mkdir Library
-fi
-
-cd Library
-
-if [ ! -d "Extensions" ]; then
-    sudo mkdir Extensions
-fi
-
-cd Extensions
-
-#
-# Creating directories for AppleHDA.kext
-#
-
-if [ ! -d "AppleHDA.kext/Contents/MacOS" ]; then
-    sudo mkdir -p AppleHDA.kext/Contents/MacOS
-fi
-
-cd AppleHDA.kext/Contents
-
-#
-# Copy AppleHDA executable into FileGuard. 
-#
-
-sudo cp -p /System/Library/Extensions/AppleHDA.kext/Contents/MacOS/AppleHDA MacOS/
-
-if [ ! -d "Resources" ]; then
-    sudo mkdir -p Resources
-fi
-
-#
-# Create FileGuard backup of the audio (XML) files.
-#
-
-sudo cp -p /System/Library/Extensions/AppleHDA.kext/Contents/Resources/layout${LAYOUT}.xml Resources/
-sudo cp -p /System/Library/Extensions/AppleHDA.kext/Contents/Resources/Platforms.xml Resources/
-
-if [ ! -d "PlugIns/AppleHDAHardwareConfigDriver.kext/Contents" ]; then
-    sudo mkdir -p PlugIns/AppleHDAHardwareConfigDriver.kext/Contents 
-fi
-
-cd PlugIns/AppleHDAHardwareConfigDriver.kext/Contents
-sudo cp -p /System/Library/Extensions/AppleHDA.kext/Contents/PlugIns/AppleHDAHardwareConfigDriver.kext/Contents/Info.plist .
-
-cd /Extra/FileGuard/Files/System/Library/Extensions
-
-#
-# ========= START OF ASUS SPECIFIC BOARD SUPPORT =========
-#
-
-# 
-# Create directories for AppleIntelCPUPowerManagement.kext (when missing).
-#
-
-if [ ! -d "AppleIntelCPUPowerManagement.kext/Contents/MacOS" ]; then
-    sudo mkdir -p AppleIntelCPUPowerManagement.kext/Contents/MacOS
-fi
-
-sudo cp -p /System/Library/Extensions/AppleIntelCPUPowerManagement.kext/Contents/MacOS/AppleIntelCPUPowerManagement AppleIntelCPUPowerManagement.kext/Contents/MacOS
-
-#
-# ========== END OF ASUS SPECIFIC BOARD SUPPORT ==========
-#
-
-#
-# Create directories for IONetworkingFamily.kext (when missing).
-#
-
-if [ ! -d "IONetworkingFamily.kext/Contents/PlugIns" ]; then
-    sudo mkdir -p IONetworkingFamily.kext/Contents/PlugIns
-fi
-
-cd IONetworkingFamily.kext/Contents/PlugIns
-
-#
-# Copy kext (plugin) for the network adapter(s) into FileGuard.
-#
-
-if [ -d "/System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/AppleIntelE1000e.kext" ]; then
-    sudo cp -Rp /System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/AppleIntelE1000e.kext .
-fi
-
-if [ -d "/System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/AppleYukon2.kext" ]; then
-    sudo cp -Rp /System/Library/Extensions/IONetworkingFamily.kext/Contents/PlugIns/AppleYukon2.kext .
-fi
-
-cd /Extra/FileGuard/Files/System/Library/Extensions
-
-#
-# Copy FakeSMC.kext into FileGuard.
-#
-
-if [ ! -d "FakeSMC.kext/Contents/MacOS" ]; then    
-    sudo cp -Rp /System/Library/Extensions/FakeSMC.kext .
-fi
-
-#
-# ============= START OF ATI GRAPHICS SUPPORT ============
-#
-
-#
-# Create directories for ATI6000Controller.kext (when missing).
-#
-
-if [ ! -d "ATI6000Controller.kext/Contents/MacOS" ]; then
-    sudo mkdir -p ATI6000Controller.kext/Contents/MacOS
-fi
-
-cd ATI6000Controller.kext/Contents/MacOS
-
-#
-# Copy modified kext for the ATI graphics card into FileGuard.
-#
-
-sudo cp -Rp /System/Library/Extensions/ATI6000Controller.kext/Contents/MacOS/ATI6000Controller .
-
-#
-# ============== END OF ATI GRAPHICS SUPPORT =============
-#
-
-cd /Extra
-
-echo "\nDone"
+exit 0
