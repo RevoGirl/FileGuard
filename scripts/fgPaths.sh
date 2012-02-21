86870fd8b4c36cf97540b213ff4a956520b63746
Samantha <dutchhockeygoalie@yahoo.com>
2012-02-21 05:12:49 +0100

    Delete bug fixed, sync added, comments changed

--- scripts/fgPaths.sh
+++ scripts/fgPaths.sh
@@ -3,16 +3,22 @@
 #
 # Administrator shell script (fgPaths.sh) to control the FileGuard WatchPaths
 #
-# Version 0.6 - Copyright (c) 2012 by RevoGirl <DutchHockeyGoalie@yahoo.com>
+# Version 0.7 - Copyright (c) 2012 by RevoGirl <DutchHockeyGoalie@yahoo.com>
 #
 # Contributors: Geoff (STLVNUB) who helped me with _setLayoutID()
 #
 
-#set -x # Used for tracing errors
+#set -x # Used for tracing errors (can be put anywhere in the script).
 
 #================================= GLOBAL VARS ==================================
 
+DAEMON_LABEL=com.fileguard.watcher
+
 fgConfigPlist=/Extra/FileGuard/com.fileguard.config.plist
+
+fgTmpLaunchDaemonPlist=/tmp/com.fileguard.watcher.plist
+
+fgLaunchDaemonPlist=/Library/LaunchDaemons/com.fileguard.watcher.plist
 
 #=============================== LOCAL FUNCTIONS ================================
 
@@ -77,6 +83,35 @@ function _initWatchTargets()
 
 #--------------------------------------------------------------------------------
 
+function _initLaunchDaemonPlist()
+{
+  echo "Creating: $fgTmpLaunchDaemonPlist"
+  echo "------------------------------------------------------------"
+  echo '<?xml version="1.0" encoding="UTF-8"?>'                  > $fgTmpLaunchDaemonPlist
+  echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> $fgTmpLaunchDaemonPlist
+  echo '<plist version="1.0">'                                  >> $fgTmpLaunchDaemonPlist
+  echo '<dict>'                                                 >> $fgTmpLaunchDaemonPlist
+  echo '    <key>Label</key>'                                   >> $fgTmpLaunchDaemonPlist
+  echo '    <string>com.fileguard.watcher</string>'             >> $fgTmpLaunchDaemonPlist
+  echo '    <key>ProgramArguments</key>'                        >> $fgTmpLaunchDaemonPlist
+  echo '    <array>'                                            >> $fgTmpLaunchDaemonPlist
+  echo '        <string>/Extra/FileGuard/Daemon/daemon</string>'>> $fgTmpLaunchDaemonPlist
+  echo '    </array>'                                           >> $fgTmpLaunchDaemonPlist
+  echo '    <key>RunAtLoad</key>'                               >> $fgTmpLaunchDaemonPlist
+  echo '    <false/>'                                           >> $fgTmpLaunchDaemonPlist
+  echo '    <key>WatchPaths</key>'                              >> $fgTmpLaunchDaemonPlist
+  echo '    <array>'                                            >> $fgTmpLaunchDaemonPlist
+  echo '    </array>'                                           >> $fgTmpLaunchDaemonPlist
+  echo '    <key>StandardErrorPath</key>'                       >> $fgTmpLaunchDaemonPlist
+  echo '    <string>/var/log/FileGuardDaemon.log</string>'      >> $fgTmpLaunchDaemonPlist
+  echo '    <key>StandardOutPath</key>'                         >> $fgTmpLaunchDaemonPlist
+  echo '    <string>/var/log/FileGuardDaemon.log</string>'      >> $fgTmpLaunchDaemonPlist
+  echo '</dict>'                                                >> $fgTmpLaunchDaemonPlist
+  echo '</plist>'                                               >> $fgTmpLaunchDaemonPlist
+}
+
+#--------------------------------------------------------------------------------
+
 function _fileExists()
 {
   if [ -e $1 ]; 
@@ -100,19 +135,27 @@ function _setFilePermissions()
 function _doCmdAdd()
 {
   local found=0
+  local target=$1
 
   _doCmdInit quiet # Silently creates the missing plist.
 
   local currentWatchPaths=(`defaults read ${fgConfigPlist} WatchPaths | tr '\n' ' ' | sed 's/(//;s/ *//g;s/\"//g;s/,/ /g;s/)//'`)
 
+  #
+  # This piece of code won't be entered after a delete command (empty WatchPaths).
+  #
   if [ ${#currentWatchPaths[@]} -gt 0 ]; then
+      #
+      # Remove redundant part (default target directory) from start of path.
+      #
+      local target=${1#/System/Library/Extensions/}
       #
       # Check current items, to prevent duplicates.
       #
       for element in $(seq 0 $((${#currentWatchPaths[@]} - 1)))
       do
-          if [ "${currentWatchPaths[$element]}" == "$1" ]; then
-              echo "Error: Watch path already listed: ${1}"
+          if [ "${currentWatchPaths[$element]}" == "$target" ]; then
+              echo "Error: Watch path already listed: ${target}"
               local found=1
               break
           fi
@@ -120,7 +163,7 @@ function _doCmdAdd()
   fi
 
   if [ $found -eq 0 ]; then
-      `defaults write ${fgConfigPlist} WatchPaths -array-add ${1%quick}`
+      `defaults write ${fgConfigPlist} WatchPaths -array-add ${target%quick}`
       #
       # Skipped when called from _doCmdSetup (speedup the process).
       #
@@ -144,12 +187,18 @@ function _doCmdConvert()
 
           if [[ $ret =~ ^Binary ]];
               then
+                  #
+                  # No output when parameter 'quiet' is given.
+                  #
                   if [ ! $1 ]; then
                       echo "Config file converted to XML plist format"
                   fi
                   
                   `/usr/bin/sudo /usr/bin/plutil -convert xml1 ${fgConfigPlist}`
               else
+                  #
+                  # No output when parameter 'quiet' is given.
+                  #
                   if [ ! $1 ]; then
                       echo "Config file converted to binary plist format"
                   fi
@@ -186,7 +235,7 @@ function _doCmdDelete()
           if [ $found -eq 1 ];
               then
                   `/usr/bin/sudo /usr/bin/defaults delete $fgConfigPlist WatchPaths`
-                  local new="${newWatchPaths[@]}"  # Bah!
+                  local new="${newWatchPaths[@]}" # Bah!
                   _doCmdAdd "$new"
                   _doCmdShow
               else
@@ -202,32 +251,39 @@ function _doCmdDelete()
 function _doCmdInit()
 {
   if [ $(_fileExists $fgConfigPlist) -eq 0 ];
-    then
-      if [ ! $1 ]; then
-        echo "Creating: ${fgConfigPlist} (empty / no watch paths yet)"
-        echo 'Use: "./fgPaths.sh add <path>" to add a watch path'
-      fi
+      then
+          #
+          # No output when parameter 'quiet' is given.
+          #
+          if [ ! $1 ]; then
+              echo "Creating: ${fgConfigPlist} (empty / no watch paths yet)"
+              echo 'Use: "./fgPaths.sh add <path>" to add a watch path'
+          fi
 
-      echo '<?xml version="1.0" encoding="UTF-8"?>'			 > $fgConfigPlist
-      echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> $fgConfigPlist
-      echo '<plist version="1.0">'					>> $fgConfigPlist
-      echo '<dict>'							>> $fgConfigPlist
-      echo '    <key>WatchPaths</key>'					>> $fgConfigPlist
-      echo '    <array>'						>> $fgConfigPlist
-      echo '    </array>'						>> $fgConfigPlist
-      echo '</dict>'							>> $fgConfigPlist
-      echo '</plist>'							>> $fgConfigPlist
-      _setFilePermissions
-    else
-      if [ ! $1 ]; then
-        echo "Command ignored (${fgConfigPlist} is already there)"
-      fi
+          echo '<?xml version="1.0" encoding="UTF-8"?>'			 > $fgConfigPlist
+          echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> $fgConfigPlist
+          echo '<plist version="1.0">'					>> $fgConfigPlist
+          echo '<dict>'							>> $fgConfigPlist
+          echo '    <key>WatchPaths</key>'				>> $fgConfigPlist
+          echo '    <array>'						>> $fgConfigPlist
+          echo '    </array>'						>> $fgConfigPlist
+          echo '</dict>'						>> $fgConfigPlist
+          echo '</plist>'						>> $fgConfigPlist
+
+          _setFilePermissions
+      else
+          #
+          # No output when parameter 'quiet' is given.
+          #
+          if [ ! $1 ]; then
+              echo "Command ignored (${fgConfigPlist} is already there)"
+          fi
   fi
 }
 
 #--------------------------------------------------------------------------------
 
-function _doCmdSetup()
+function _doCmdSetup
 {
   #
   # Check if config file exists, if yes then wipe it, else create the file. 
@@ -246,7 +302,6 @@ function _doCmdSetup()
   #
   for element in $(seq 0 $((${#fgDefaultWatchList[@]} -1)))
   do
-      # `defaults write ${fgConfigPlist} WatchPaths -array-add ${fgDefaultWatchList[$element]}`
       _doCmdAdd ${fgDefaultWatchList[$element]} quick
   done
 
@@ -272,6 +327,58 @@ function _doCmdShow()
       else
           echo "Error: Watch list is (still) empty"
   fi
+}
+
+#--------------------------------------------------------------------------------
+
+function _doCmdSync()
+{
+  #
+  # Read the target watch paths from our configuration plist.
+  #
+  local watchPaths=(`defaults read ${fgConfigPlist} WatchPaths | tr '\n' ' ' | sed 's/(//;s/ *//g;s/\"//g;s/,/ /g;s/)//'`)
+  #
+  # Catching the 'bad array subscript' error (when the array is empty).
+  #
+  if [ ${#watchPaths[@]} -gt 0 ];
+      then
+          #
+          # Create a new (temporarily) file in /tmp
+          #
+          _initLaunchDaemonPlist
+          #
+          # We now have an empty file so let's sync the watch paths.
+          #
+          for element in $(seq 0 $((${#watchPaths[@]} -1)))
+          do
+              `defaults write ${fgTmpLaunchDaemonPlist} WatchPaths -array-add ${watchPaths[$element]}`
+          done
+  fi
+  #
+  # Convert the file to human 'readable' XML format.
+  #
+  `/usr/bin/sudo /usr/bin/plutil -convert xml1 ${fgTmpLaunchDaemonPlist}`
+  #
+  # Fix file ownership and permissions.
+  #
+  `/usr/bin/sudo /bin/chmod 644 ${fgTmpLaunchDaemonPlist}`
+  `/usr/bin/sudo /usr/sbin/chown root:wheel ${fgTmpLaunchDaemonPlist}`
+  #
+  # Move synced file file to the right spot.
+  #
+  `/usr/bin/sudo /bin/cp -p $fgTmpLaunchDaemonPlist $fgLaunchDaemonPlist`
+  #
+  # Stopping the launch daemon.
+  #
+  `/usr/bin/sudo /bin/launchctl stop ${DAEMON_LABEL}`
+  #
+  # Unloading the launch daemon.
+  #
+  `/usr/bin/sudo /bin/launchctl unload /Library/LaunchDaemons/${DAEMON_LABEL}.plist`
+  #
+  # Reloading the launch daemon.
+  #
+  `/usr/bin/sudo /bin/launchctl load /Library/LaunchDaemons/${DAEMON_LABEL}.plist`
 }
 
 #--------------------------------------------------------------------------------
@@ -310,13 +417,17 @@ function _main()
             _doCmdShow
             ;;
 
+        sync)
+            _doCmdSync
+            ;;
+
         wipe)
             `/usr/bin/sudo /usr/bin/defaults delete $fgConfigPlist WatchPaths`
             ;;
 
         *)
 
-        echo $"Usage: $0 {add <watch path>|convert|delete <watch path>|init|list|setup <layout-id>|show|wipe}"
+        echo $"Usage: $0 {add <watch path>|convert|delete <watch path>|init|list|setup <layout-id>|show|sync|wipe}"
         exit 1
   esac
 }
@@ -329,7 +440,7 @@ function _main()
 
 function _isRoot()
 {
-  if [ $(id -u) != 0 ]; then
+  if [ $(id -u) -ne 0 ]; then
       echo "This script must be run as root" 1>&2
       exit 1
   fi
